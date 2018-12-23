162a3aa8f872a978237297951d1a5f24502988cf
Sabique <sabiquekhan@gmail.com>
2018-12-23 15:24:54 +0530

    Removed the code wrote for the Action Results and Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,26 +15,6 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
 
             return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound("This is an error message.");
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id =" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("Page Index = {0} & Sort By = {1}", pageIndex, sortBy));
         }
     }
 }
